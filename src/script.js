c1e9544280e73aa34831c2f716dc568112be3f89
Nusrat1002 <nusrat.j.jannat103@gmail.com>
2025-07-18 00:54:57 +0600

    Book part completed

--- src/script.js
+++ src/script.js
@@ -3,3 +3,14 @@ let menu = document.querySelector(".menu");
 navBtn.addEventListener("click", function(){
     menu.classList.toggle("toggle")
 });
+
+const trip = document.querySelector('.Trip');
+  const comment = document.querySelector('.comment');
+
+  trip.addEventListener('mouseenter', () => {
+    comment.style.opacity = 1;
+  });
+
+  trip.addEventListener('mouseleave', () => {
+    comment.style.opacity = 0;
+  });
